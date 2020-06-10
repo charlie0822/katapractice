188b2d778b554a0e2dc9f3a580c9ee71f588ab25
charlie0822 <charlie80037@gmail.com>
2020-06-10 10:09:31 +0800

    kata2

---
+++ Kata/Kata/kata2/Kata2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.kata2
+{
+    class Kata2
+    {
+        public static int DeadAntCount(string ants)
+        {
+            int countnumber = 0;
+            if (ants == null)
+            {
+                return 0;
+            }
+            string[] stringsplit = ants.Split(' ');
+
+            foreach (var ant in stringsplit)
+            {
+                for (int i = 0; i < stringsplit.Length; i++)
+                {
+                    if (ant[i].Equals("a"))
+                    {
+                        countnumber++;
+                    }
+
+
+                }
+
+            }
+
+            return countnumber;
+        }
+    }
+}
---
+++ Kata/Kata/kata2/samepletest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Kata.kata2
+{
+    [TestFixture]
+    class sampletest
+    {
+        [Test]
+        public void samepletest()
+        {
+
+            Assert.AreEqual(0, Kata2.DeadAntCount("ant ant ant ant"));
+            Assert.AreEqual(0, Kata2.DeadAntCount(null));
+            Assert.AreEqual(2, Kata2.DeadAntCount("ant anantt aantnt"));
+            Assert.AreEqual(1, Kata2.DeadAntCount("ant ant .... a nt"));
+        }
+    }
+}
